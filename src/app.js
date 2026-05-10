dce3ed8457bfed1b8301b1ae8cc8d6c4741c888f
Danilo <danilo.campioni@gmail.com>
2026-05-10 11:09:07 -0300

    feat: configura documentação Swagger na API

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 import { fastify } from 'fastify';
 import { videosRoutes } from './routes/videos-routes.js';
+import swagger from '@fastify/swagger';
+import swaggerUI from '@fastify/swagger-ui';
 
 const app = fastify();
+
+app.register(swagger, {
+    openapi: {
+        info: {
+            title: 'Fastify Video API',
+            description: 'API desenvolvida com Node.js, Fastify e PostgreSQL',
+            version: '1.0.0'
+        }
+    }
+});
+
+app.register(swaggerUI, {
+    routePrefix: '/docs'
+});
+
 app.register(videosRoutes);
 
 export { app }
